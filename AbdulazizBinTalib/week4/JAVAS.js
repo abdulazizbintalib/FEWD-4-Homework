79e810cddb97b893a953a6e53b42cd48e1d9d06e
abdulazizbintalib <56229407+abdulazizbintalib@users.noreply.github.com>
2019-11-05 11:11:33 +0300

    Add files via upload

---
+++ AbdulazizBinTalib/week4/JAVAS.js
@@ -0,0 +1,21 @@
+var num=1755;
+if(num % 3 == 0 && num%5 == 0)
+{
+    console.log('PlingPlang');
+}
+else if(num%5 == 0)
+{
+    console.log('Plang');
+}
+else if(num%7 == 0)
+{
+    console.log('Plong');
+}
+else if(num%3 == 0)
+{
+    console.log('Pling');
+}
+else 
+{
+    console.log('34')
+}
